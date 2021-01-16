5bc6d3b7b3a704bab04c0736b05998c8857b909c
Ernesto Jara Olveda <ernesto.jara06@gmail.com>
2021-01-15 20:34:46 -0600

    3-parte

---
+++ config/webpack.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const webpack = require("webpack");
+
+const vars = require("./env");
+const { NODE_ENV = "development" } = process.env;
+const isProd = NODE_ENV === "production";
+const entry = [path.join(__dirname, NODE_ENV === "production" ? `../src/prod.ts` : `../src/dev.ts`)];
+
+module.exports = {
+    target: "node",
+    name: "server",
+    externals: [nodeExternals()],
+    bail: isProd,
+    entry,
+    devtool: false,
+    mode: process.env.NODE_ENV,
+    output: {
+        filename: "[name]-bundle.js",
+        chunkFilename: "[name].chunk.js",
+        path: path.resolve(__dirname, "../bundle"),
+        publicPath: "/",
+        libraryTarget: "commonjs2",
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [new webpack.DefinePlugin(vars.stringified)],
+};
